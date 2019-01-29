16519ee52d55d460475e4f17e5faa47ee88bbb8a
dyankastutara <dyankastutara19@gmail.com>
2019-01-29 11:49:33 +0700

    default convert to web

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import HybridApp from './src/App';
+
+
+export default class App extends Component<Props> {
+  render() {
+    return <HybridApp />
+  }
+}
